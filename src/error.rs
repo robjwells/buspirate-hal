6f36e21d4de885f35d32d7c24a461faa65983030
Rob Wells <rob@robjwells.com>
2025-07-24 19:29:56 +0100

    Organise modules into separate files

---
+++ src/error.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+pub enum Error {
+    SerialPort(serialport::Error),
+    Io(std::io::Error),
+    Flatbuffer(flatbuffers::InvalidFlatbuffer),
+    Other,
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::Io(value)
+    }
+}
+
+impl From<flatbuffers::InvalidFlatbuffer> for Error {
+    fn from(value: flatbuffers::InvalidFlatbuffer) -> Self {
+        Self::Flatbuffer(value)
+    }
+}
+
+impl From<serialport::Error> for Error {
+    fn from(value: serialport::Error) -> Self {
+        Self::SerialPort(value)
+    }
+}
+
+impl embedded_hal::i2c::Error for Error {
+    fn kind(&self) -> embedded_hal::i2c::ErrorKind {
+        // TODO: This is a dummy implementation.
+        embedded_hal::i2c::ErrorKind::Other
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // TODO: This is a dummy implementation.
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
